a9e52d3c01df4c2fb84737c41b6b43d8780e022f
devRaxx <rpcastaneros@up.edu.ph>
2024-03-22 11:57:17 +0800

    Added image link

--- src/InfoCard.jsx
+++ src/InfoCard.jsx
@@ -18,6 +18,12 @@ function InfoCard() {
         overflow="hidden"
         variant="outline"
       >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src="https://scontent.fdvo3-1.fna.fbcdn.net/v/t1.15752-9/277314471_1411449252652273_5940717447462598210_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=nJUsmUps7bIAX940cFq&_nc_oc=AdgBdPyh4H-K9yTx-k8pxWsVnVttb9W7nWeZ1wEJRn7c7k90y-LAERwrEKWp3gwcWNo&_nc_ht=scontent.fdvo3-1.fna&oh=03_AdRY1yUdvt3MNf1SiHrlh3Qy1Amc917MPpAV9L2ZqWv54w&oe=66248CD2"
+          alt=""
+        />
         <Stack>
           <CardBody className="text-start">
             <Heading size="md">Rocky Angelo P. Castañeros</Heading>
